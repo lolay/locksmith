d50d1ef95ef38fcc6f1e4f789e69cd91c8b7e246
Gary Rudolph <GaryRudolph@me.com>
2011-12-14 21:37:37 -0800

    Fixed some formatting and headers

--- NSUserDefaults+LolayBase64.h
+++ NSUserDefaults+LolayBase64.h
@@ -1,14 +1,13 @@
 //
-//  NSUserDefaults+LolayBase64.h
-//  MyLifePhone
-//
-//  Created by Feng Wu on 12/13/11.
-//  Copyright (c) 2011 Lolay Inc. All rights reserved.
+//  Created by Lolay, Inc.
+//  Copyright 2011 Lolay, Inc. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
 
 @interface NSUserDefaults (LolayBase64)
+
 - (void) setCodableObject:(id<NSCoding>) value forKey:(NSString*) defaultName;
-- (id<NSCoding>) codableObjectForKey:(NSString *) defaultName;
+- (id<NSCoding>) codableObjectForKey:(NSString*) defaultName;
+
 @end
--- NSUserDefaults+LolayBase64.m
+++ NSUserDefaults+LolayBase64.m
@@ -1,9 +1,6 @@
 //
-//  NSUserDefaults+LolayBase64.m
-//  MyLifePhone
-//
-//  Created by Feng Wu on 12/13/11.
-//  Copyright (c) 2011 Lolay Inc. All rights reserved.
+//  Created by Lolay, Inc.
+//  Copyright 2011 Lolay, Inc. All rights reserved.
 //
 
 #import "NSUserDefaults+LolayBase64.h"
@@ -11,16 +8,16 @@
 #import "NSString+LolayBase64.h"
 
 @implementation NSUserDefaults (LolayBase64)
+
 - (void) setCodableObject:(id<NSCoding>) value forKey:(NSString*) defaultName {
-    NSData *data = [NSKeyedArchiver archivedDataWithRootObject:value];
-    NSString *string = [NSData base64StringWithData:data];
+    NSData* data = [NSKeyedArchiver archivedDataWithRootObject:value];
+    NSString* string = [NSData base64StringWithData:data];
     [[NSUserDefaults standardUserDefaults] setObject:string forKey:defaultName];
 }
 
 - (id<NSCoding>) codableObjectForKey:(NSString *) defaultName {
-    
-    NSString *string = [[NSUserDefaults standardUserDefaults] objectForKey:defaultName];
-    NSData *data = [NSString base64DataWithString:string];
+    NSString* string = [[NSUserDefaults standardUserDefaults] objectForKey:defaultName];
+    NSData* data = [NSString base64DataWithString:string];
     id<NSCoding> obj = [NSKeyedUnarchiver unarchiveObjectWithData:data];
     return obj;
 }
