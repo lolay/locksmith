a26ee0e2786c956b6cd9124e4c97d20c8f1c89c9
GaryRudolph <GaryRudolph@mac.com>
2012-04-12 14:39:45 -0700

    Updated license for compliance

--- NSData+LolayHmacSha1.m
+++ NSData+LolayHmacSha1.m
@@ -1,6 +1,17 @@
 //
-//  Created by Lolay, Inc.
-//  Copyright 2011 Lolay, Inc. All rights reserved.
+//  Copyright 2012 Lolay, Inc. All rights reserved.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
 //
 
 #import <CommonCrypto/CommonHMAC.h>
@@ -8,7 +19,6 @@
 
 @implementation NSData (LolayHmacSha1)
 
-// From http://stackoverflow.com/questions/735714/iphone-and-hmac-sha-1-encoding
 - (NSData*) hmacSha1DataWithKey:(NSData*) key {
     void* buffer = malloc(CC_SHA1_DIGEST_LENGTH);
     CCHmac(kCCHmacAlgSHA1, [key bytes], [key length], [self bytes], [self length], buffer);
